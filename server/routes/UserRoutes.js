f632db90d1c9048a07c7703db7e866020162f70f
SanjanaTiwari06 <sanjanat0609@gmail.com>
2026-06-05 19:30:19 +0530

    Update UserRoutes.js

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -15,14 +15,14 @@ const {
     forgetPassword3,
 } = require("../controllers/UserController")
 
-UserRouter.post("", authPublic, createRecord)
+UserRouter.post("", createRecord)
 UserRouter.get("", authBuyer, getRecord)
 UserRouter.get("/:_id", authBuyer, getSingleRecord)
 UserRouter.put("/:_id", authBuyer, updateRecord)
 UserRouter.delete("/:_id", authBuyer, deleteRecord)
-UserRouter.post("/login", authPublic, login)
-UserRouter.post("/forget-password-1", authPublic, forgetPassword1)
-UserRouter.post("/forget-password-2", authPublic, forgetPassword2)
-UserRouter.post("/forget-password-3", authPublic, forgetPassword3)
+UserRouter.post("/login",  login)
+UserRouter.post("/forget-password-1", forgetPassword1)
+UserRouter.post("/forget-password-2",forgetPassword2)
+UserRouter.post("/forget-password-3",  forgetPassword3)
 
 module.exports = UserRouter
