1452d5d8aa1e67bf38e806332a79744baafcc222
SanjanaTiwari06 <sanjanat0609@gmail.com>
2026-06-05 18:36:21 +0530

    Fix CORS origin URL by removing trailing slash

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://apni-dukan-uak1.vercel.app/"  // Fixed: removed trailing slash
+    "https://apni-dukan-uak1.vercel.app"  // Fixed: removed trailing slash
   ]
 }));
 
